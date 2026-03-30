0fad28f3deab02d97f8577b2daf6377c164489f5
Tommy Cheng <tommycheng51@gmail.com>
2026-03-30 19:51:54 +0800

    Change cafe name and tagline

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,13 @@
 <!-- resources/views/partials/header.blade.php -->
 <header class="site-header">
-    <div class="container">
-        <h1>🎲 Dice & Dine Boardgame Café</h1>
-        <p class="tagline">Play · Eat · Adventure</p>
+    <div class="header-content">
+        <div class="brand-section">
+            <div class="logo-placeholder">📖</div>
+            <div class="brand-text">
+                <h1>Fable</h1>
+                <p class="tagline">Every game tells a story.</p>
+            </div>
+        </div>
+        @include('partials.navbar')
     </div>
-    <br>
-    @include('partials.navbar')
 </header>
