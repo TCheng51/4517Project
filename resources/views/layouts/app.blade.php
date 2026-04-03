8fff6209c6d206778e7e5837ebb5d2450dcca702
Tommy Cheng <tommycheng51@gmail.com>
2026-04-03 18:25:10 +0800

    Simplify webpage structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Fabel | Every game tells a story' }}</title>
-    @vite(['resources/css/style.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
     @include('partials.header')
